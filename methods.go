1f876e31d2cfae3a09eba629628ea2af606c16ab
SANGMINLEEE <dlaak0630@naver.com>
2019-11-25 00:10:36 +0900

    methods 2

--- methods.go
+++ methods.go
@@ -19,6 +19,10 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func Abs2(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
