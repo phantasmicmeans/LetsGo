27cd8e020e5422e688241e524c955012617efd0d
SANGMINLEEE <dlaak0630@naver.com>
2019-11-25 00:08:08 +0900

    methods

---
+++ methods.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+/**
+A Method is a function with a special receiver argument.
+The receiver appears in its own argument list
+between "func" keyword and the method name.
+*/
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
