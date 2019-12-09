d5db5270a2e3d6cc4d76cf50c32f6a22c43bd74a
SANGMINLEEE <dlaak0630@naver.com>
2019-12-09 13:34:00 +0900

    interface-value with nil

--- interface-values.go
+++ interface-values.go
@@ -18,6 +18,7 @@ import (
 
 type I interface {
 	M()
+	M2()
 }
 
 type T struct {
@@ -36,17 +37,44 @@ func (f F) M() {
 	fmt.Println(f)
 }
 
+func (t *T) M2() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(t.S)
+}
+
+func (f F) M2() {
+	fmt.Println(f)
+}
+
 func describe(i I) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
 
 func main() {
 	var i I
-	i = &T{"Hello"}
+	i = &T{"Hello M()"}
 	describe(i)
 	i.M()
 
 	i = F(math.Pi)
 	describe(i)
 	i.M()
+
+	/* Interface values with nil underlying values
+	If the concrete value inside the interface itself is nil.
+	the method will be called with a nil receiver.
+
+	In some languages this would trigger a null pointer exception,
+	but in Go it is common to wirte methods that gracefully handle being called
+	with a nil receiver.
+
+	Note that an interface value that holds a nil concrete value is itself non-nil.
+	*/
+	var i2 I
+	i2 = &T{"Hello M2()"}
+	describe(i2)
+	i2.M2()
 }
